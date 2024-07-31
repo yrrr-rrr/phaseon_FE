26a07b79d00fcb4f8b54490963a944d158362e54
seoyerin <seyerin12@naver.com>
2024-07-31 23:22:50 +0900

    feat(#3) 이전 커밋 내역이 남아있음
    더미 데이터 작성
    
    더미 데이터 수정
    
    feat(#3) ProjectDetail commponent 구현
    
    projectIntro component 생성 및 스타일 적용
    
    svg 이미지 추가 및 ProjectDetails comoponent 스타일 수정
    
    라우터 연결
    
    상세페이지 사용 이미지 다운로드
    
    console.log 삭제

--- src/interface.ts
+++ src/interface.ts
@@ -24,3 +24,96 @@ export interface SgvPropsType {
   height: string;
   name: string;
 }
+
+export interface Button {
+  icon: string;
+  buttontext: string;
+  link: string;
+}
+
+export interface CarouselItem {
+  img: string;
+  type: string;
+}
+
+export interface MainFeature {
+  title: string;
+  description: string;
+}
+
+export interface ReleaseVersion {
+  version: number;
+  extrafeature: string[];
+}
+
+export interface CertifiedAward {
+  badge: string;
+  award: string;
+}
+
+export interface NewsItem {
+  img: string;
+  title: string;
+  content: string;
+}
+
+export interface ProfileLink {
+  icon: string;
+  link: string;
+}
+
+export interface Member {
+  img: string;
+  role: string;
+  email: string;
+  profilelink: ProfileLink[];
+}
+
+export interface MainProjectInfo {
+  carousel: CarouselItem[];
+  mainfeatures: {
+    shortdescription: string;
+    feature: MainFeature[];
+  };
+  techstack: string[];
+}
+
+export interface FloatMenu {
+  category: string;
+  buttons: Button[];
+  releasenote: string;
+  performance: string[];
+  members: string[];
+}
+
+export interface Intro {
+  banner: string;
+  projectimg: string;
+  projectbrief: string;
+  description: string;
+}
+
+export interface Performance {
+  certifiedaward: CertifiedAward[];
+  accomplishment: string[];
+  news: NewsItem[];
+}
+
+export interface Introduction {
+  members: Member[];
+}
+
+export interface DetailDataType {
+  projectname: string;
+  star: number;
+  floatmenu: FloatMenu;
+  intro: Intro;
+  category: {
+    projectinfo: MainProjectInfo;
+    release: {
+      releaseversion: ReleaseVersion[];
+    };
+    performance: Performance;
+    introduction: Introduction;
+  };
+}
---
+++ src/style/projectDetail/projectIntroStyle.tsx
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+import { ReactComponent as AttiMainImg } from '../../assets/svg/AttiMainImg.svg';
+export const Section = styled.section`
+  width: 1100px;
+  height: 414px;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+`;
+
+export const IntroSection = styled.section`
+  width: 740px;
+  height: 334px;
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 80px;
+  flex: none;
+`;
+
+export const MainImg = styled(AttiMainImg)`
+  margin-bottom: 24px;
+  width: 160px;
+  border-radius: 4px;
+  box-shadow:
+    0px 5.405px 21.62px 0px rgba(0, 0, 0, 0.12),
+    0px 2.702px 10.81px 0px rgba(0, 0, 0, 0.08),
+    0px 0px 2.702px 0px rgba(0, 0, 0, 0.08);
+`;
+
+export const ProjectName = styled.span`
+  font-size: 36px;
+  font-weight: 700;
+  line-height: 133.4%;
+  letter-spacing: -0.972px;
+`;
+
+export const ProjectBrief = styled.p`
+  margin-bottom: 16px;
+  color: #858588;
+  font-size: 22px;
+  font-weight: 400;
+  line-height: 136.4%;
+  letter-spacing: -0.427px;
+`;
+
+export const Description = styled.p`
+  color: #171719;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 162.5%;
+  letter-spacing: 0.091px;
+`;
