d6691a0dbb012411bfaf04c4b74582550489746b
seoyerin <seyerin12@naver.com>
2024-08-06 22:56:18 +0900

    StarButton, ShareButton component 사이즈 props로 받아서 적용
    버튼 사이즈에 따라 폰트 사이즈 조절
    
    StarButton, ShareButton component props로 사이즈 조절

--- src/components/common/ShareButton.tsx
+++ src/components/common/ShareButton.tsx
@@ -1,9 +1,11 @@
 import * as s from '../../style/projectDetail/common/ButtonStyle';
 import { ReactComponent as Share } from '../../assets/svg/Share.svg';
 
-export default function ShareButton() {
+export default function ShareButton({ width, height }: { width: number; height: number }) {
   return (
     <s.Button
+      $width={width}
+      $height={height}
       $border={'1px solid #69ACFF'}
       $color={'69acff'}
       $type={'share'}
