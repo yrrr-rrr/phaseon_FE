80b3c047d105d1c6cc1eeb91517f22a71e4ae873
seoyerin <seyerin12@naver.com>
2024-08-09 02:48:50 +0900

    fix(#36): 캐러셀 무한루프 수정 및 slide button 개수 수정

--- src/components/projectDetail/Carousel.tsx
+++ src/components/projectDetail/Carousel.tsx
@@ -11,6 +11,11 @@ export default function Carousel() {
   const { data } = useContext(ProjectDetailContext);
   const [currentImg, setCurrentImg] = useState<number>(0);
 
+  const slides =
+    data.category.projectinfo.carousel.length <= 2
+      ? [...data.category.projectinfo.carousel, ...data.category.projectinfo.carousel]
+      : data.category.projectinfo.carousel;
+
   const onSelect = useCallback(() => {
     if (!emblaApi) return;
     setCurrentImg(emblaApi.selectedScrollSnap());
@@ -26,12 +31,12 @@ export default function Carousel() {
     <s.Section>
       <s.PrevButton
         onClick={() => {
-          emblaApi?.scrollNext();
+          emblaApi?.scrollPrev();
         }}
       />
       <s.CarouselViewport ref={emblaRef}>
         <s.CarouselContainer>
-          {data.category.projectinfo.carousel.map((carouselObj, key) => {
+          {slides.map((carouselObj, key) => {
             if (carouselObj.type == 'video') {
               return (
                 <s.CarouselSlide key={carouselObj.img + key}>
@@ -48,23 +53,26 @@ export default function Carousel() {
               return (
                 <s.CarouselSlide key={carouselObj.img + key}>
                   <s.Img src={`/public/${carouselObj.img}`} alt="" $type={carouselObj.type} />
-                  {/* imgType={carouselObj.type} */}
                 </s.CarouselSlide>
               );
             }
           })}
         </s.CarouselContainer>
         <s.ButtonSection>
-          {data.category.projectinfo.carousel.map((carouselObj, index) => (
-            <s.SlideButton
-              aria-label="btn"
-              key={index}
-              $bgColor={currentImg == index ? 'color' : 'none'}
-              onClick={() => {
-                emblaApi?.scrollTo(index);
-              }}
-            ></s.SlideButton>
-          ))}
+          {slides.map((carouselObj, index) => {
+            if (index < slides.length / 2) {
+              return (
+                <s.SlideButton
+                  aria-label="btn"
+                  key={index}
+                  $bgColor={currentImg == index ? 'color' : 'none'}
+                  onClick={() => {
+                    emblaApi?.scrollTo(index);
+                  }}
+                ></s.SlideButton>
+              );
+            }
+          })}
         </s.ButtonSection>
       </s.CarouselViewport>
       <s.NextButton
