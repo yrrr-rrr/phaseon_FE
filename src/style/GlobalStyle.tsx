1db963606fa6e7d6d0ddc761e52816f4b6ef374a
seoyerin <seyerin12@naver.com>
2024-08-09 04:04:22 +0900

    폰트 적용

--- src/style/GlobalStyle.tsx
+++ src/style/GlobalStyle.tsx
@@ -1,10 +1,17 @@
 import { createGlobalStyle } from 'styled-components';
+import Pretendard_Variable from './font/PretendardJPVariable.ttf';
 
 export const GlobalStyle = createGlobalStyle`
+@font-face {
+  font-family: 'Pretendard';
+  src: url(${Pretendard_Variable})format("woff");
+  font-style: normal;
+}
+
   *{
     margin: 0;
     padding: 0;
-    
+    font-family: 'Pretendard';
   }
   body{
     width: 100%;
