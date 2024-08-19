44afa5f8bcabf8841c37d0fb10e019214f2720ed
seoyerin <seyerin12@naver.com>
2024-08-20 01:12:45 +0900

    HandleBox 구현 완료

---
+++ src/components/projectDetail/Zoom/HandleBox.tsx
@@ -0,0 +1,24 @@
+import { useContext } from 'react';
+import { ZoomContext } from '../../../context/ZoomContext';
+import * as s from '../../../style/projectDetail/ZoomimgStyle';
+
+export default function HandleBox() {
+  const { setShowZoomComponent, setIsZoomed, setZoomCount, updateTransform } = useContext(ZoomContext);
+
+  return (
+    <s.HandelBox>
+      <s.Close
+        onClick={() => {
+          document.body.style.overflow = 'auto';
+          setZoomCount(1);
+          updateTransform((transformObj) => {
+            transformObj.x = 0;
+            transformObj.y = 0;
+          });
+          setShowZoomComponent(false);
+          setIsZoomed(false);
+        }}
+      />
+    </s.HandelBox>
+  );
+}
