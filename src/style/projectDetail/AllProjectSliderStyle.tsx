dce7259267454005897205e095bf4ea44debe641
seoyerin <seyerin12@naver.com>
2024-08-28 19:15:30 +0900

    AllProjectSlider 이미지 box-shadow 추가

--- src/style/projectDetail/AllProjectSliderStyle.tsx
+++ src/style/projectDetail/AllProjectSliderStyle.tsx
@@ -54,12 +54,18 @@ export const CarouselContainer = styled.div`
 export const CarouselSlide = styled.div`
   margin: 0 16px;
   height: 304px;
+  /* box-shadow: 10px 10px 10px black; */
   cursor: pointer;
 `;
 
 export const Img = styled.img`
   width: 240px;
   height: 240px;
+  border-radius: 8px;
+  box-shadow:
+    0px 1px 2px 0px rgba(0, 0, 0, 0.12),
+    0px 0px 1px 0px rgba(0, 0, 0, 0.08),
+    0px 0px 1px 0px rgba(0, 0, 0, 0.08);
 `;
 
 export const ProjectName = styled.p`
