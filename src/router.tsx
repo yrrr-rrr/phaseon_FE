b5beb30d26ddae3b4d33df34f4ac965a2d983f56
seoyerin <seyerin12@naver.com>
2024-09-16 19:54:09 +0900

    라우터 적용

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 import ProjectDetails from './page/ProjectDetails';
-import ProjectGallery from './page/ProjectGallery';
+import Main from './page/Main';
 
 export const router = createBrowserRouter([
   {
-    path: '/project/:projectName',
+    path: '/:projectName',
     element: <ProjectDetails />,
   },
   {
-    path: '/project',
-    element: <ProjectGallery />,
+    path: '/',
+    element: <Main />,
   },
 ]);
