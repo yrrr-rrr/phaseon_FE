6f6d15e950c3c85f0e91c65c6915ac2d85b2d40a
seoyerin <seyerin12@naver.com>
2024-09-20 01:22:46 +0900

    chore: 아이콘 동적 매핑 설정

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from '@svgr/rollup';
 import path from 'path';
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // @를 src로 매핑
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
