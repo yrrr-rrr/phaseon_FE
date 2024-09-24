e976db2c7343acb6573427d9fdd38c75f0c9d1ca
seoyerin <seyerin12@naver.com>
2024-09-24 15:39:36 +0900

    refactor: url id 값 프로젝트 명으로 변경

--- src/components/main/AllProjects.tsx
+++ src/components/main/AllProjects.tsx
@@ -10,42 +10,45 @@ export default function AllProjects() {
   return (
     <s.ProjectContainer>
       {project &&
-        project.data.projects.map((projectObj, idx) => (
-          <s.ProjectBox
-            key={idx}
-            onClick={() => {
-              window.scrollTo({
-                top: 0,
-                behavior: 'smooth',
-              });
-              navigate(`/${projectObj.id}`);
-            }}
-          >
-            <s.Img src={projectObj.thumbnail} alt="" />
-            <div>
-              <s.ProjectName>Project Name</s.ProjectName>
-              <s.InpoBox>
-                <s.IconBox>
-                  <Icon name="FillStar" fill="#FFCC00" />
-                  <p>{projectObj.likeCount}</p>
-                </s.IconBox>
-                <s.IconBox>
-                  <Icon name="Medal" />
-                  <p>{projectObj.awardCount}</p>
-                </s.IconBox>
-                <s.IconBox>
-                  <Icon name="Member" fill="#69ACFF" width={16} height={12} />
-                  <p>{projectObj.memberCount}</p>
-                </s.IconBox>
-              </s.InpoBox>
-              <s.CategoryBox>
-                {projectObj.category.map((categoryText, idx2) => (
-                  <p key={idx2 + categoryText}>{categoryText}</p>
-                ))}
-              </s.CategoryBox>
-            </div>
-          </s.ProjectBox>
-        ))}
+        project.data.projects.map((projectObj, idx) => {
+          const param = projectObj.title.toLowerCase().replace(' ', '_');
+          return (
+            <s.ProjectBox
+              key={idx}
+              onClick={() => {
+                window.scrollTo({
+                  top: 0,
+                  behavior: 'smooth',
+                });
+                navigate(`/${param}`);
+              }}
+            >
+              <s.Img src={projectObj.thumbnail} alt="" />
+              <div>
+                <s.ProjectName>Project Name</s.ProjectName>
+                <s.InpoBox>
+                  <s.IconBox>
+                    <Icon name="FillStar" fill="#FFCC00" />
+                    <p>{projectObj.likeCount}</p>
+                  </s.IconBox>
+                  <s.IconBox>
+                    <Icon name="Medal" />
+                    <p>{projectObj.awardCount}</p>
+                  </s.IconBox>
+                  <s.IconBox>
+                    <Icon name="Member" fill="#69ACFF" width={16} height={12} />
+                    <p>{projectObj.memberCount}</p>
+                  </s.IconBox>
+                </s.InpoBox>
+                <s.CategoryBox>
+                  {projectObj.category.map((categoryText, idx2) => (
+                    <p key={idx2 + categoryText}>{categoryText}</p>
+                  ))}
+                </s.CategoryBox>
+              </div>
+            </s.ProjectBox>
+          );
+        })}
     </s.ProjectContainer>
   );
 }
